00f8c40f903fb71bcb645e4573d6c9bc39eeba3c
wonsss <59413128+wonsss@users.noreply.github.com>
2021-09-08 17:02:04 +0900

    ampm

--- js/clock.js
+++ js/clock.js
@@ -4,16 +4,16 @@ const dayArray = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
 
 function getClock() {
   const date = new Date();
-  const hours = String(date.getHours()).padStart(2, "0");
+  const hours = date.getHours() > 12 ? `PM ${String(date.getHours()).padStart(2, "0")-12}` : `AM ${String(date.getHours()).padStart(2, "0")}`;
   const minutes = String(date.getMinutes()).padStart(2, "0");
   // const seconds = String(date.getSeconds()).padStart(2, "0");
   const year = date.getFullYear();
   const month = String(date.getMonth() - 1).padStart(2, "0");
   const dateNum = String(date.getDate()).padStart(2, "0");
-  const ampm = hours>12 ? 'PM' : 'AM';
   const dayIndex = date.getDay();
-  clock.innerText = `${ampm} ${hours}:${minutes}`;
+  clock.innerText = `${hours}:${minutes}`;
   today.innerText = `${year}. ${month}. ${dateNum}. ${dayArray[dayIndex]}`;
+
 }
 getClock();
 setInterval(getClock, 1000);
