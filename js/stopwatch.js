ceee32841b581bdb62c5a3e2c9390e0a88b2f89c
wonsss <59413128+wonsss@users.noreply.github.com>
2021-09-08 21:09:03 +0900

    css

--- js/stopwatch.js
+++ js/stopwatch.js
@@ -57,7 +57,7 @@ function resetWatch() {
 function getClock() {
   const date = new Date();
   const hours = date.getHours() > 12 ? `PM ${String(date.getHours()-12).padStart(2, "0")}` : `AM ${String(date.getHours()).padStart(2, "0")}`;
-  const minutes = String(date.getMinutes()).padStart("0");
+  const minutes = String(date.getMinutes()).padStart(2,"0");
   const month = String(date.getMonth() - 1);
   const dateNum = String(date.getDate());
   return (`The last time you focused was ${hours}:${minutes}(${month}. ${dateNum}.).`)
